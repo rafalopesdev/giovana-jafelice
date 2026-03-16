1e5e54dc0923e87295e37748f50b7f47b9b07f22
Rafael Lopes <79181915+rafalopesdev@users.noreply.github.com>
2026-03-16 20:25:29 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+async function loadComponents() {
+  const tasks = [
+    loadComponent('header-placeholder', '/components/header.html'),
+    loadComponent(
+      'footer-contato-placeholder',
+      '/components/footer-contato.html',
+    ),
+    loadComponent('footer-placeholder', '/components/footer.html'),
+  ];
+
+  // Agora ele vai esperar todas as tarefas que REALMENTE existem
+  await Promise.all(tasks);
+  document.body.classList.add('loaded');
+}
+
+function loadComponent(id, path) {
+  const element = document.getElementById(id);
+
+  // SEGREDO: Se o ID não existir nesta página, avisamos que a tarefa acabou (resolve)
+  if (!element) {
+    return Promise.resolve();
+  }
+
+  return fetch(path)
+    .then((response) => {
+      if (!response.ok) throw new Error(`Erro ao carregar ${path}`);
+      return response.text();
+    })
+    .then((data) => {
+      element.innerHTML = data;
+    })
+    .catch((err) => {
+      console.warn('Aviso:', err.message);
+    });
+}
+
+loadComponents();
